b35010383ed505e32b7536286070756e192adb6f
Renato_O <renatorsdeoliveira@gmail.com>
2019-10-12 23:18:24 -0300

    Criado a classe debug e colocado para debugar a classe MainActivity

--- app/src/main/java/mobile/fasam/edu/exemplodebug/MainActivity.java
+++ app/src/main/java/mobile/fasam/edu/exemplodebug/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends DebugActivity {
 
     EditText txtNome;
     EditText txtTelefone;
